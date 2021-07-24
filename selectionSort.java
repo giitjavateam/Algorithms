b1a3225f5a5eb5cc4b3e67ef1aa35a9e241e2912
ritz-293 <singhreetika.2000@gmail.com>
2021-07-24 12:55:43 +0530

    SortSelectionSort

---
+++ selectionSort.java
@@ -0,0 +1,41 @@
+
+public class selectionSort {
+	
+	void sort(int arr[]) {
+		int n = arr.length;
+		for(int i = 0; i<n-1 ; i++)
+		{
+			int min = i;
+			for(int j= i+1; j<n ;j++)
+			{
+				if(arr[min] > arr[j]) {
+					min = j;
+				}
+			}
+			int temp = arr[min];
+			arr[min] = arr[i];
+			arr[i] = temp;
+		}
+	}
+	
+	void printArray(int arr[])
+	{
+		int n = arr.length;
+		for (int i=0; i<n; ++i)
+			System.out.print(arr[i] + " ");
+		System.out.println();
+	}
+
+	
+	public static void main(String args[])
+	{
+		bubbleSort ob = new bubbleSort();
+		int arr[] = {64, 34, 25, 12, 22, 11, 90};
+		System.out.println("Unsorted array");
+		ob.printArray(arr);
+		ob.Sort(arr);
+		System.out.println("Sorted array");
+		ob.printArray(arr);
+	}
+
+}
